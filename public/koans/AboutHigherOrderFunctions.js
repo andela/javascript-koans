8543a8ada2be22bd96024ea501bbdfed3a3493c4
gnerkus <ifeanyioraelosi@gmail.com>
2014-12-22 16:08:26 +0100

    Refactor Jasmine specs
    Refactor AboutHigherOrderFunctions.js and AboutMutability.js to have an assert per test.

--- public/koans/AboutHigherOrderFunctions.js
+++ public/koans/AboutHigherOrderFunctions.js
@@ -7,84 +7,168 @@ var _; //globals
  */
 describe("About Higher Order Functions", function () {
 
-  it("should use filter to return array items that meet a criteria", function () {
-    var numbers = [1,2,3];
-    var odd = _(numbers).filter(function (x) { return x % 2 !== 0 });
+    var odd,
+        numbers,
+        numbersPlus1,
+        reduction,
+        msg,
+        isEven,
+        onlyEven,
+        mixedBag,
+        result;
 
-    expect(odd).toEqual(FILL_ME_IN);
-    expect(odd.length).toBe(FILL_ME_IN);
-    expect(numbers.length).toBe(FILL_ME_IN);
-  });
+    describe("Using 'filter'", function () {
 
-  it("should use 'map' to transform each element", function () {
-    var numbers = [1, 2, 3];
-    var numbersPlus1 = _(numbers).map(function(x) { return x + 1 });
+        beforeEach(function () {
+            numbers = [1,2,3];
+            odd = _(numbers).filter(function (x) { return x % 2 !== 0 });
+        });
 
-    expect(numbersPlus1).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
-  });
+        it("should have an array that meets a criteria", function () {
+            expect(odd).toEqual(FILL_ME_IN);
+        });
 
-  it("should use 'reduce' to update the same result on each iteration", function () {
-    var numbers = [1, 2, 3];
-    var reduction = _(numbers).reduce(
-            function(/* result from last call */ memo, /* current */ x) { return memo + x }, /* initial */ 0);
+        it("should have have the correct array", function () {
+            expect(odd.length).toBe(FILL_ME_IN);
+        });
 
-    expect(reduction).toBe(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
-  });
+        it("should assert the original array is untouched", function () {
+            expect(numbers.length).toBe(FILL_ME_IN);
+        });
+    });
 
-  it("should use 'forEach' for simple iteration", function () {
-    var numbers = [1,2,3];
-    var msg = "";
-    var isEven = function (item) {
-      msg += (item % 2) === 0;
-    };
 
-    _(numbers).forEach(isEven);
+    describe("Using 'map'", function () {
 
-    expect(msg).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
-  });
+        beforeEach(function () {
+            numbers = [1, 2, 3];
+            numbersPlus1 = _(numbers).map(function(x) { return x + 1 });
+        });
 
-  it("should use 'all' to test whether all items pass condition", function () {
-    var onlyEven = [2,4,6];
-    var mixedBag = [2,4,5,6];
+        it("should transform each element", function () {
+            expect(numbersPlus1).toEqual(FILL_ME_IN);
+        });
 
-    var isEven = function(x) { return x % 2 === 0 };
+        it("should assert the original array is untouched", function () {
+            expect(numbers).toEqual(FILL_ME_IN);
+        })
+    });
 
-    expect(_(onlyEven).all(isEven)).toBe(FILL_ME_IN);
-    expect(_(mixedBag).all(isEven)).toBe(FILL_ME_IN);
-  });
 
-  it("should use 'any' to test if any items passes condition" , function () {
-    var onlyEven = [2,4,6];
-    var mixedBag = [2,4,5,6];
+    describe("Using 'reduce'", function () {
 
-    var isEven = function(x) { return x % 2 === 0 };
+        beforeEach(function () {
+            numbers = [1, 2, 3];
+            reduction = _(numbers).reduce(
+                    function(/* result from last call */ memo, /* current */ x) { return memo + x }, /* initial */ 0);
+        });
 
-    expect(_(onlyEven).any(isEven)).toBe(FILL_ME_IN);
-    expect(_(mixedBag).any(isEven)).toBe(FILL_ME_IN);
-  });
+        it("should use 'reduce' to update the same result on each iteration", function () {
+            expect(reduction).toBe(FILL_ME_IN);
+        });
 
-  it("should use range to generate an array", function() {
-      expect(_.range(3)).toEqual(FILL_ME_IN);
-      expect(_.range(1, 4)).toEqual(FILL_ME_IN);
-      expect(_.range(0, -4, -1)).toEqual(FILL_ME_IN);
-  });
+        it("should assert the original array is untouched", function () {
+            expect(numbers).toEqual(FILL_ME_IN);
+        });
+    });
 
-  it("should use flatten to make nested arrays easy to work with", function() {
-      expect(_([ [1, 2], [3, 4] ]).flatten()).toEqual(FILL_ME_IN);
-  });
 
-  it("should use chain() ... .value() to use multiple higher order functions", function() {
-      var result = _([ [0, 1], 2 ]).chain()
-                       .flatten()
-                       .map(function(x) { return x+1 } )
-                       .reduce(function (sum, x) { return sum + x })
-                       .value();
+    describe("Using 'forEach'", function () {
 
-      expect(result).toEqual(FILL_ME_IN);
-  });
+        beforeEach(function () {
+            numbers = [1,2,3];
+            msg = "";
+            isEven = function (item) {
+              msg += (item % 2) === 0;
+            };
+
+            _(numbers).forEach(isEven);
+        });
+
+        it("should use 'forEach' for simple interaction", function () {
+            expect(msg).toEqual(FILL_ME_IN);
+        });
+
+        it("should assert the original array is untouched", function () {
+            expect(numbers).toEqual(FILL_ME_IN);
+        });
+    });
+
+
+    describe("Using 'all'", function () {
+
+        beforeEach(function () {
+            onlyEven = [2,4,6];
+            mixedBag = [2,4,5,6];
+            isEven = function(x) { return x % 2 === 0 };
+        });
+
+        it("should use 'all' to test whether all items pass condition", function () {
+            expect(_(onlyEven).all(isEven)).toBe(FILL_ME_IN);
+        });
+
+        it("should use 'all' to test whether all items pass condition", function () {
+            expect(_(mixedBag).all(isEven)).toBe(FILL_ME_IN);
+        });
+
+    });
+
+
+    describe("Using 'any'", function () {
+
+        beforeEach(function () {
+            onlyEven = [2,4,6];
+            mixedBag = [2,4,5,6];
+            isEven = function(x) { return x % 2 === 0 };
+        });
+
+        it("should use 'any' to test if any items pass condition", function () {
+            expect(_(onlyEven).any(isEven)).toBe(FILL_ME_IN);
+        });
+
+        it("should use 'all' to test if any items pass condition", function () {
+            expect(_(mixedBag).any(isEven)).toBe(FILL_ME_IN);
+        });
+
+    });
+
+
+    describe("Generating arrays with 'range'", function () {
+
+        it("should use range to generate an array from one argument", function () {
+            expect(_.range(3)).toEqual(FILL_ME_IN);
+        });
+
+        it("should use range to generate an array from two arguments", function () {
+            expect(_.range(1, 4)).toEqual(FILL_ME_IN);
+        });
+
+        it("should use range to generate an array from three arguments", function () {
+            expect(_.range(0, -4, -1)).toEqual(FILL_ME_IN);
+        });
+    });
+
+
+    describe("Using flatten.", function () {
+
+        it("should use flatten to make nested arrays easy to work with", function () {
+            expect(_([ [1, 2], [3, 4] ]).flatten()).toEqual(FILL_ME_IN);
+        });
+    });
+
+
+    describe("Using chain for multiple higher order functions", function () {
+        beforeEach(function () {
+            result = _([ [0, 1], 2 ]).chain()
+                             .flatten()
+                             .map(function(x) { return x+1 } )
+                             .reduce(function (sum, x) { return sum + x })
+                             .value();
+        });
+
+        it("should use chain() ... .value() to use multiple higher order functions", function () {
+            expect(result).toEqual(FILL_ME_IN);
+        });
+    });
 
 });
-
--- public/koans/AboutMutability.js
+++ public/koans/AboutMutability.js
@@ -1,68 +1,96 @@
 describe("About Mutability", function() {
 
-  it("should expect object properties to be public and mutable", function () {
-    var aPerson = {firstname: "John", lastname: "Smith" };
-    aPerson.firstname = "Alan";
+    var aPerson;
 
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
-  });
+    describe("Object Properties", function () {
+        beforeEach(function () {
+            aPerson = {firstname: "John", lastname: "Smith" };
+            aPerson.firstname = "Alan";
+        });
 
-  it("should understand that constructed properties are public and mutable", function () {
-    function Person(firstname, lastname)
-    {
-      this.firstname = firstname;
-      this.lastname = lastname;
-    }
-    var aPerson = new Person ("John", "Smith");
-    aPerson.firstname = "Alan";
+        it("should expect object properties to be public and mutable", function () {
+            expect(aPerson.firstname).toBe(FILL_ME_IN);
+        });
+    });
 
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
-  });
 
-  it("should expect prototype properties to be public and mutable", function () {
-    function Person(firstname, lastname)
-    {
-      this.firstname = firstname;
-      this.lastname = lastname;
-    }
-    Person.prototype.getFullName = function () {
-      return this.firstname + " " + this.lastname;
-    };
+    describe("Constructed Properties", function () {
+        beforeEach(function () {
+            function Person(firstname, lastname)
+            {
+              this.firstname = firstname;
+              this.lastname = lastname;
+            }
+            aPerson = new Person ("John", "Smith");
+        });
 
-    var aPerson = new Person ("John", "Smith");
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+        it("should understand that constructed properties are public and mutable", function () {
+            aPerson.firstname = "Alan";
+            expect(aPerson.firstname).toBe(FILL_ME_IN);
+        });
+    });
 
-    aPerson.getFullName = function () {
-      return this.lastname + ", " + this.firstname;
-    };
 
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
-  });
+    describe("Prototype Properties", function () {
+        beforeEach(function () {
+            function Person(firstname, lastname)
+            {
+              this.firstname = firstname;
+              this.lastname = lastname;
+            }
+            Person.prototype.getFullName = function () {
+              return this.firstname + " " + this.lastname;
+            };
+        });
 
-  it("should know that variables inside a constructor and constructor args are private", function () {
-    function Person(firstname, lastname)
-    {
-      var fullName = firstname + " " + lastname;
+        it("should expect prototype properties to be public", function () {
+            aPerson = new Person ("John", "Smith");
+            expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+        });
 
-      this.getFirstName = function () { return firstname; };
-      this.getLastName = function () { return lastname; };
-      this.getFullName = function () { return fullName; };
-    }
-    var aPerson = new Person ("John", "Smith");
+        it("should expect prototype properties to be mutable", function () {
+            aPerson.getFullName = function () {
+              return this.lastname + ", " + this.firstname;
+            };
+            expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+        });
+    });
 
-    aPerson.firstname = "Penny";
-    aPerson.lastname = "Andrews";
-    aPerson.fullName = "Penny Andrews";
+    describe("Constructor Variables", function () {
+        beforeEach(function () {
+            function Person(firstname, lastname)
+            {
+              var fullName = firstname + " " + lastname;
 
-    expect(aPerson.getFirstName()).toBe(FILL_ME_IN);
-    expect(aPerson.getLastName()).toBe(FILL_ME_IN);
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+              this.getFirstName = function () { return firstname; };
+              this.getLastName = function () { return lastname; };
+              this.getFullName = function () { return fullName; };
+            }
+            aPerson = new Person ("John", "Smith");
+        });
 
-    aPerson.getFullName = function () {
-      return aPerson.lastname + ", " + aPerson.firstname;
-    };
+        it("should know that constructor args are private", function () {
+            aPerson.firstname = "Penny";
+            expect(aPerson.getFirstName()).toBe(FILL_ME_IN);
+        });
 
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
-  });
+        it("should know that constructor args are private", function () {
+            aPerson.lastname = "Andrews";
+            expect(aPerson.getLastName()).toBe(FILL_ME_IN);
+        });
+
+        it("should know that variables inside a constructor are private", function () {
+            aPerson.fullName = "Penny Andrews";
+            expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+        });
+
+        it("should know that variables inside a constructor are private", function () {
+            aPerson.getFullName = function () {
+              return aPerson.lastname + ", " + aPerson.firstname;
+            };
+
+            expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+        });
+    });
 
 });
