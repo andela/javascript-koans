f87ae98eba374d8e13df50060f85258b3c10c04c
andela-bojengwa <bernardojengwa@gmail.com>
2014-12-31 13:42:32 +0100

    Refactored AboutArray.js
    Separated each specs with multiple ‘expects’ into a suite with each
    ‘expect’ rewritten as a spec.

--- public/koans/AboutArrays.js
+++ public/koans/AboutArrays.js
@@ -1,9 +1,8 @@
 //We shall contemplate truth by testing reality, via spec expectations.
-describe("About Arrays", function() {
+describe("About Arrays: ", function() {
 
   isArray = function (obj) {
     if (typeof(obj) === 'object' && obj.hasOwnProperty('length')) {
-      console.log(typeof(obj));
       return true;
     }
     return false;
@@ -22,29 +21,29 @@ describe("About Arrays", function() {
       });
     });
 
-  // describe('Array Values', function() {
+  describe('Array Values', function() {
 
-  //   var multiTypeArray = [0, 1, "two", function () { return 3; }, {value1: 4, value2: 5}, [6, 7]],
+    var multiTypeArray = [0, 1, "two", function () { return 3; }, {value1: 4, value2: 5}, [6, 7]];
 
 
-  //   it('should check array value', function() {
-  //     expects(multiTypeArray[FILL_KEY_IN]).toBe(FILL_ME_IN);
-  //   });
+    it('should check array value', function() {
+      expects(multiTypeArray[2]).toBe(FILL_ME_IN);
+    });
 
-  //   it('should check the return value of the function in an array', function() {
-  //     expects(multiTypeArray[FILL_KEY_IN]()).toEqual(FILL_ME_IN);
-  //   });
+    it('should check the return value of the function in an array', function() {
+      expects(multiTypeArray[3]()).toEqual(FILL_ME_IN);
+    });
 
-  //   it('should check for nested cases', function() {
-  //     expect(multiTypeArray[FILL_ME_IN][FILL_ME_IN]).toEqual(FILL_ME_IN);
-  //   });
+    it('should check for nested cases', function() {
+      expect(multiTypeArray[5][0]).toEqual(FILL_ME_IN);
+    });
 
-  // });
+  });
 
-  describe('Array Length', function() {
+  describe('Array Length: ', function() {
 
       var fourNumberArray = [1,2,3,4],
-         tenEmptyElementArray = new Array(10);
+          tenEmptyElementArray = new Array(10);
 
     it('should understand array length', function(){
       expect(fourNumberArray.length).toEqual(FILL_ME_IN);
@@ -65,5 +64,101 @@ describe("About Arrays", function() {
     });
   });
 
+
+  describe('Array Slicing: ', function() {
+
+    beforeEach(function() {
+      var array = ["peanut", "butter", "and", "jelly"];
+    });
+
+    it('should return only the first element in the array', function() {
+      expect(array.slice(0,1)).toBe(FILL_ME_IN);
+    });
+
+    it('should return at least one element form the array', function() {
+      expect(array.slice(0, 2)).toBe(FILL_ME_IN);
+    });
+
+    it('should return an empty array', function() {
+      expect(array.slice(2,2)).toBe(FILL_ME_IN);
+    });
+
+    it('should return all element in the array from the start index', function() {
+      expect(array.slice(2, 20)).toBe(FILL_ME_IN);
+    });
+
+    it('should be an empty array', function() {
+      expect(array.slice(3,0)).toBe(FILL_ME_IN);
+    });
+
+  });
+
+  describe('Array References: ', function() {
+
+    var passedByReference = function (refArray) {
+        refArray[1] = "changed in function";
+    }
+
+    beforeEach(function() {
+      var array = [ "zero", "one", "two", "three", "four", "five" ];
+    });
+
+    it('should know array references', function() {
+      passedByReference(array);
+      expect(array[1]).toBe(FILL_ME_IN);
+    });
+
+    it('should pass by reference', function() {
+      var assignedArray = array;
+      assignedArray[5] = "changed in copyOfArray";
+      expect(array[5]).toBe(FILL_ME_IN);
+    });
+
+    it('should understand array copy', function() {
+      var copyOfArray = array.slice();
+      copyOfArray[3] = "changed in copyOfArray";
+      expect(array[3]).toBe(FILL_ME_IN);
+    });
+
+  });
+
+  describe('Array Push and Pop: ', function() {
+
+    beforeEach(function() {
+      var array = [1,2];
+    })
+
+    it('should understand push', function() {
+      array.push(3);
+      expect(array).toEqual(FILL_ME_IN);
+    });
+
+    it('should understand return the popped value', function() {
+      var poppedValue = array.pop();
+      expect(poppedValue).toBe(FILL_ME_IN);
+    });
+
+    it('should show any changes in array', function() {
+      expect(array).toEqual(FILL_ME_IN);
+    });
+  });
+
+  describe('Array shift and unshift: ', function() {
+    var array = [1,2];
+
+    it('should know about shifting arrays', function() {
+      array.unshift(3);
+      expect(array).toEqual(FILL_ME_IN);
+    });
+
+    it('should know about the return value for an array unshifting operation', function() {
+      var shiftedValue = array.shift();
+      expect(shiftedValue).toEqual(FILL_ME_IN);
+    });
+
+    it('should know what value remains in a shifted array', function() {
+      expect(array).toEqual(FILL_ME_IN);
+    }); 
+  });
 
 });
